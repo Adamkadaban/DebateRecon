d72c672ad0708a2b7a8ebb2537221fbf775762e2
Adam Hassan <34610663+Adamkadaban@users.noreply.github.com>
2020-01-19 22:10:00 -0500

    Added loading screen and better interface

--- DebateRecon.py
+++ DebateRecon.py
@@ -7,48 +7,30 @@
 #
 #  A program to be used at NSDA Districts and similar tournaments with codes in order to collect team codes
 #  https://github.com/Adamkadaban
-#  https://github.com/Nate8888
 #
-#  Adam Hassan & Nathan Wilk
+#  Adam Hassan
 #
-#  Version 1.0 alpha 2/9/19
-
-
+#  Version 2.0 beta 1/19/20
 
 #take from end of Tabroom URL if you know how code works
 #also comment out the input lines
-
-
-#import stuff#
-try:
-    from lxml import html
-    import requests
-    import argparse
-except ImportError as msg:
-    print("[!] Library not installed: " + str(msg))
-    os.system('python -m pip install requests')
-    os.system('python -m pip install argparse')
-    os.system('python -m pip install lxml')
-    exit()
-parser = argparse.ArgumentParser()
-
-
-parser.add_argument("-t", "--tournament", type=str, help="target link", required=True)
-parser.add_argument("-r", "--round", type=str, help="checkbox option id", required=True)
-
-args = parser.parse_args()
-
-
-tourn_id=args.tournament
-round_id=args.round
+tourn_id="11946"
+round_id="400524"
 Lookup=""
+
+choose=True
+if choose:
+  tourn_id=input("Input the tournament ID (from the link): ")
+  round_id=input("Input the round ID (from the link): ")
+  Lookup=input("If you want only one school, type its name here. Otherwise, press enter ")
+
 #put in school name if you want only one school in the output
 
 ##Send me prep if you are thankful that you don't have to do this manually##
 
 ##Start of the code that debaters should not look at##
-
-
+from lxml import html
+import requests, threading, sys, time
 # tourn_id=input("Input the tournament ID from the end of the tabroom link\n")
 # round_id=input("Input the round ID from the end to the tabroom link\n")
 # Lookup=input("If you want to look up a specific school or team, enter the name. Otherwise, press enter\n")
@@ -74,19 +56,21 @@ def secondRequest(urls):
     data=[]
     if Lookup in school:
       try:
-        print(number +'{:26}'.format(fun[0])+'{:26}'.format(fun[1]) +'{:26}'.format(fun[2])+school)
+        data=(number,fun[0],fun[1],fun[2],school)
       except:
         try:
-            print(number +'{:26}'.format(fun[0])+'{:26}'.format(fun[1]) +"\t"+school)
+            data=[number,fun[0],fun[1],school]
         except:
-            print(number + '{:26}'.format(+fun[0])+school)
+            data=[number,fun[0],school]
+    return data
 
 
-url = "https://www.tabroom.com/index/tourn/postings/round.mhtml"
-querystring = {"tourn_id":tourn_id,"round_id":round_id}
+def runner():
+  url = "https://www.tabroom.com/index/tourn/postings/round.mhtml"
+  querystring = {"tourn_id":tourn_id,"round_id":round_id}
 
 
-headers = {
+  headers = {
     'upgrade-insecure-requests': "1",
     'user-agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
     'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
@@ -94,13 +78,61 @@ headers = {
     'accept-language': "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7",
     'cookie': "_ga=GA1.2.114986879.1549734299; _gid=GA1.2.784529514.1549734299; _ga=GA1.2.114986879.1549734299; _gid=GA1.2.784529514.1549734299",
     'cache-control': "no-cache",
-    }
+  }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+  response = requests.request("GET", url, headers=headers, params=querystring)
 
-webpage = html.fromstring(response.content)
-buyers = webpage.xpath('//a[@class="white smallish padtop padbottom padleft"]/@href') #This gives me all URLS in the page
-for x in range(len(buyers)):
+  webpage = html.fromstring(response.content)
+  buyers = webpage.xpath('//a[@class="white smallish padtop padbottom padleft"]/@href') #This gives me all URLS in the page
+  data=[]
+  for x in range(len(buyers)):
     if not "judge" in buyers[x]: #Some urls were not teams, they were judges urls. Therefore Im sorting here
-        string = "https://www.tabroom.com"+buyers[x] #Create string to send 2nd request to fetch data
-        secondRequest(string) #MEthod that fetches data
+      string = "https://www.tabroom.com"+buyers[x] #Create string to send 2nd request to fetch data
+      data.append(secondRequest(string)) #MEthod that fetches data
+  
+  data=sorted(data, key=lambda x: x[len(x)-1])
+  for i in data:
+    code=i[0]
+    name1=i[1]
+    name2=""
+    name3=""
+    try:
+      name3=i[3]
+      name2=i[2]
+    except:
+      name3=i[2]
+    if name2=="":
+      print(code +'{:26}'.format(name1)+'{:26}'.format(name3))
+    else:
+      print(code +'{:26}'.format(name1)+'{:26}'.format(name2) +'{:26}'.format(name3))
+
+
+
+
+def loading():
+    sys.stdout.write("\rLoading .")
+    time.sleep(.1)
+    sys.stdout.flush()
+    sys.stdout.write("\rLoading ..")
+    time.sleep(.1)
+    sys.stdout.flush()
+    sys.stdout.write("\rLoading ...")
+    time.sleep(.1)
+    sys.stdout.flush()
+    sys.stdout.write("\rLoading  ..")
+    time.sleep(.1)
+    sys.stdout.flush()
+    sys.stdout.write("\rLoading   .")
+    time.sleep(.1)
+    sys.stdout.flush()
+
+
+
+t1=threading.Thread(target=runner)
+
+t1.start()
+
+while t1.isAlive():
+  loading()
+
+
